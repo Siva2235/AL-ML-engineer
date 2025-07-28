f2d7c6dfea91038242b5ae60d99e8449e3fd3808
SivaKumar978 <sol.siva79@gmail.com>
2025-07-28 13:13:37 +0530

    Initial commit

---
+++ sum.py
@@ -0,0 +1,8 @@
+# Your First Python Program
+name = input("Enter your name: ")
+print(f"Hello, {name}! Welcome to my AI/ML journey!")
+
+# Basic arithmetic
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print(f"Sum = {a + b}")
